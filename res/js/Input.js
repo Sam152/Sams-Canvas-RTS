3c7c249092c65d4b9eeb91ee11579c3793ed585e
Sam <sam152@live.com.au>
2013-03-21 21:50:17 +0800

    Initial version of the Input object.

--- res/js/Input.js
+++ res/js/Input.js
@@ -3,18 +3,45 @@
  * Capture input from a users keyboard and expose it cleanly.
  */
 var Input = klass(function() {
-
-  this.keyStates = {};
-
 });
 
 
 Input.statics({
 
-  'getMousePosition' : function() {
+  /**
+   * Return the state of an individual key.
+   */
+  'getKeyState' : function(key_code) {
+    return key_code in this.keyStates
+      ? this.keyStates[key_code]
+      : false;
   },
 
-  'getKeyState' : function(key_code) {
+  /**
+   * Start capturing the keyboard events and populating our keyStates object.
+   */
+  'startCapture' : function() {
+    var self = this;
+    this.keyStates = {};
+
+    function updateKeyState(e) {
+      self.keyStates[e.keyCode] = e.type == 'keydown';
+    }
+
+    window.addEventListener('keydown', updateKeyState);
+    window.addEventListener('keyup', updateKeyState);
+  },
+
+  /**
+   * A virtual keyboard representing some commonly accessed keys and their
+   * corresponding keyCode.
+   */
+  'virtualKeyboard' : {
+    'KEY_RIGHT' : 39,
+    'KEY_LEFT' : 37,
+    'KEY_UP' : 38,
+    'KEY_DOWN' : 40
   }
+
 });
 
