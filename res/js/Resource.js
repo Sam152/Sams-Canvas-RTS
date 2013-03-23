99df450e7957609e137ddc8296dac64ca4a78bf0
Sam <samb@dhmedia.com.au>
2013-03-23 11:20:14 +0800

    Created resource function and updated Sprite function.

---
+++ res/js/Resource.js
@@ -0,0 +1,23 @@
+
+/**
+ * Something loaded from disc used by the program.
+ */
+var Resource = klass(function(settings) {
+
+  this.settings = _.extend({
+    'source' : false
+  }, settings);
+
+});
+
+
+Resource.methods({
+
+  /**
+   * Checks that a given resource exists on disk.
+   */
+  verifyResource : function() {
+    console.log('Verifying existence of: ' + this.settings.source);
+  }
+
+});
--- res/js/Sprite.js
+++ res/js/Sprite.js
@@ -2,27 +2,46 @@
 /**
  * A renderable sprite.
  */
-var Sprite = klass(function(settings) {
+var Sprite = Resource.extend(function(settings) {
 
   this.settings = _.extend({
-    'frames' : 1,
-    'frame_width' : 100,
-    'frame_height' : 100,
-    'frame_toggle_speed' : 25
-  }, settings);
+    'total_frames' : 1,
+    'frame_width' : 1,
+    'frame_height' : 1,
+    'frame_cycle_speed' : 25,
+    'canvas' : false
+  }, settings, this.settings);
+
+  this.current_frame = 0;
 
 });
 
+
 Sprite.methods({
 
+  /**
+   * Set the speed at which the animation frame changes.
+   */
+  'setFrameCycleSpeed' : function(speed) {
+    this.frame_cycle_speed = speed;
+  },
+
+  /**
+   * Render a sprite somewhere on the assigned canvas instance.
+   */
+  'render' : function(x, y) {
+
+  },
+
+  /**
+   * Create a sequence of frames to be rendered.
+   */
   'createAnimationSequence' : function() {
     var self = this;
 
     return function() {
+    
     };
   },
 
-  'setFrameToggleSpeed' : function() {
-  }
 });
-
